b86b61e1368dd66eb2242823f7d5453b675a72d0
harshey0 <sethiharshitfzr@gmail.com>
2024-06-18 09:56:54 +0530

    strings

--- Step 18 : Strings/Lec 1: Hard Problems/Z-Function.java
+++ Step 18 : Strings/Lec 1: Hard Problems/Z-Function.java
@@ -3,11 +3,8 @@ class Solution {
         
         for(int x=0;x<=a.length()-b.length();x++)
         {
-            if(a.charAt(x)==b.charAt(0))
-            {
                 if(a.substring(x,x+b.length()).equals(b))
                 return x;
-            }
         }
         return -1;
     }
