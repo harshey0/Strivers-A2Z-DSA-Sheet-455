4f0a636b1c4fcf80dc76d45df9b48594b290bac5
harshey0 <sethiharshitfzr@gmail.com>
2024-06-18 23:03:42 +0530

    trees

--- Step 13 : Binary Trees [Traversals, Medium and Hard Problems]/Lec 2: Medium Problems/Zig Zag Traversal of Binary Tree.java
+++ Step 13 : Binary Trees [Traversals, Medium and Hard Problems]/Lec 2: Medium Problems/Zig Zag Traversal of Binary Tree.java
@@ -1,10 +1,12 @@
 class Solution {
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
         Queue <TreeNode> q = new LinkedList();
         List<List<Integer>> ans = new ArrayList();
         if(root!=null)
         q.add(root);
+        int p=0;int l;
         while(!q.isEmpty())
         {
 
@@ -13,12 +15,17 @@ class Solution {
         for(int x=0;x<n;x++)
         {
             TreeNode temp = q.remove();
+            System.out.println(temp.val);
+            if(p%2==0)
             list.add(temp.val);
+            else
+            list.add(0,temp.val);
             if(temp.left!=null)
             q.add(temp.left);
             if(temp.right!=null)
             q.add(temp.right);
         }
+        p++;
         ans.add(list);
         }
         return ans;
